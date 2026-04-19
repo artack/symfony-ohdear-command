ae8a18e6246885edb9b5068d3e747ab6613e9448
Patrick Landolt <patrick.landolt@artack.ch>
2026-04-19 20:58:57 +0200

    removed nontests

--- tests/OhDearCommandTest.php
+++ tests/OhDearCommandTest.php
@@ -14,48 +14,6 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 final class OhDearCommandTest extends TestCase
 {
-    public function testExecuteWithoutUuidPassesThroughExitCode(): void
-    {
-        $command = new class extends OhDearCommand {
-            protected function configure(): void
-            {
-                parent::configure();
-                $this->setName('test:command');
-            }
-
-            protected function doExecute(InputInterface $input, OutputInterface $output): int
-            {
-                return Command::SUCCESS;
-            }
-        };
-
-        $tester = new CommandTester($command);
-        $tester->execute([]);
-
-        $this->assertSame(Command::SUCCESS, $tester->getStatusCode());
-    }
-
-    public function testExecuteWithoutUuidReturnsFailureCode(): void
-    {
-        $command = new class extends OhDearCommand {
-            protected function configure(): void
-            {
-                parent::configure();
-                $this->setName('test:command');
-            }
-
-            protected function doExecute(InputInterface $input, OutputInterface $output): int
-            {
-                return Command::FAILURE;
-            }
-        };
-
-        $tester = new CommandTester($command);
-        $tester->execute([]);
-
-        $this->assertSame(Command::FAILURE, $tester->getStatusCode());
-    }
-
     public function testOhdearUuidOptionIsRegistered(): void
     {
         $command = new class extends OhDearCommand {
@@ -72,26 +30,5 @@ final class OhDearCommandTest extends TestCase
         };
 
         $this->assertTrue($command->getDefinition()->hasOption('ohdear-uuid'));
-    }
-
-    public function testExceptionDuringExecuteWithUuidReturnsFailure(): void
-    {
-        $command = new class extends OhDearCommand {
-            protected function configure(): void
-            {
-                parent::configure();
-                $this->setName('test:command');
-            }
-
-            protected function doExecute(InputInterface $input, OutputInterface $output): int
-            {
-                throw new RuntimeException('something went wrong');
-            }
-        };
-
-        $tester = new CommandTester($command);
-        $tester->execute(['--ohdear-uuid' => 'test-uuid-that-does-not-exist']);
-
-        $this->assertSame(Command::FAILURE, $tester->getStatusCode());
     }
 }
